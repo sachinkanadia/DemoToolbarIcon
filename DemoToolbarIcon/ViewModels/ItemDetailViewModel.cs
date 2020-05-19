9818e66f4b52c12f698ef3a39f9c4182208a74f8
sachin kanadia <sachinkanadia@hotmail.com>
2020-05-19 08:34:18 +0100

    Initial check-in of module DemoToolbarIcon

---
+++ DemoToolbarIcon/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using DemoToolbarIcon.Models;
+
+namespace DemoToolbarIcon.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
